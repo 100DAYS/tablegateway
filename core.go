7ad175146f38d6cefe6c537411205236e958b23f
Gaylord Aulke <gaylord@100days.de>
2020-03-11 01:02:10 +0100

    fix postgres compatibility for fieldlist and lastInsertId

--- core.go
+++ core.go
@@ -80,24 +80,23 @@ func (dao *TableGateway) getDBFieldnames(data interface{}) string {
 		// FieldMap maps Substructs with a path-structure like: parent.child where parent is the name of the field
 		// in the main struct and child ist the name of one of the fields in the sub-struct.
 		// we will need to scan for such paths and make sure that ony the fieldnames of the main struct are returned
-		keys := make([]string, len(fields))
-		mainkeys := make(map[string]int)
+		fieldHash := make(map[string]int, len(fields))
 
 		i := 0
 		for k := range fields {
 			if strings.Contains(k, ".") {
 				parts := strings.Split(k, ".")
-				mainkeys[parts[0]] = 1
+				fieldHash[parts[0]] = 1
 			} else {
-				keys[i] = k
-				i++
+				fieldHash[k] = 1
 			}
 		}
-		for k := range mainkeys {
-			keys[i] = k
+		res := make([]string, len(fieldHash))
+		for k := range fieldHash {
+			res[i] = k
 			i++
 		}
-		dao.names = strings.Join(keys[0:i-1], ",")
+		dao.names = strings.Join(res, ",")
 	}
 	return dao.names
 }
@@ -112,12 +111,21 @@ func makePlaceholders(fields string) string {
 func (dao *TableGateway) Insert(data interface{}) (lastInsertId int64, err error) {
 	fields := dao.getDBFieldnames(data)
 	placeholders := makePlaceholders(fields)
-	q := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", dao.TableName, fields, placeholders)
-	res, err := dao.DB.NamedExec(q, data) // @todo seems we need to do this the hard way...
-	if err != nil {
-		return
+	if dao.isPostgres {
+		q := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s) RETURNING %s", dao.TableName, fields, placeholders, dao.KeyFieldName)
+		stmt, err := dao.DB.PrepareNamed(q)
+		if err != nil {
+			return 0, err
+		}
+		err = stmt.Get(&lastInsertId, data)
+	} else {
+		q := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", dao.TableName, fields, placeholders)
+		res, err := dao.DB.NamedExec(q, data)
+		if err != nil {
+			return 0, err
+		}
+		lastInsertId, err = res.LastInsertId()
 	}
-	lastInsertId, err = res.LastInsertId()
 	return
 }
 
